6b875e0496346ec929797871d672a714e1fa1c67
iamBharatManral <justbharatmanral@gmail.com>
2024-02-03 14:11:55 +0530

    feat: setup view engine handlebars with home, about, and 404 page

--- src/main.js
+++ src/main.js
@@ -1,16 +1,31 @@
 const express = require('express')
+const {engine} = require('express-handlebars')
+const {join} = require('path');
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
+app.engine('handlebars', engine())
+app.set('view engine', 'handlebars')
+app.set('views', join(__dirname, 'views'))
+
+app.use(express.static(join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
 
 app.use((req, res) => {
-    res.send('The Meadowlark Travel Website')
+    res.status(404).render('404')
 })
 
 app.use((err, req, res, next) => {
-    res.status(500)
-    res.send('500 -- Server Error')
+    console.log(err.message)
+    res.status(500).render('500')
 })
 
 app.listen(PORT, () => {
