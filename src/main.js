cfcf95ee36d05e172f7db37e88335e158ce487cc
iamBharatManral <justbharatmanral@gmail.com>
2024-02-03 21:20:44 +0530

    feat: add random fortune cookie in about page

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ const express = require('express')
 const {engine} = require('express-handlebars')
 const {join} = require('path');
 
+const fortunes = [
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know.",
+    "You will have a pleasant surprise.",
+    "Whenever possible, keep it simple.",
+]
 const app = express()
 const PORT = process.env.PORT || 4000
 
@@ -16,7 +23,8 @@ app.get('/', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+    res.render('about', {fortune: randomFortune})
 })
 
 app.use((req, res) => {
